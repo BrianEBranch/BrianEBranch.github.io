f4e2b56fc1d30cefd7e937eb83c4f8dcfeac9c77
Brian Ramos-Leiva <brianramos892@gmail.com>
2025-11-05 19:11:44 -0500

    Updates

---
+++ assets/11_5_2025-3ATb_uW7.js
@@ -0,0 +1,5 @@
+const t={id:1,title:"Tesing Blog",date:"11/5/2025",category:"SOFTWARE",readTime:"1 min",author:"BRIAN",content:`
+# TRANSMISSION BEGIN
+TESTING 123
+# TRANSMISSION END
+`};export{t as default};
